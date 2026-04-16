9c56f366cced59b6b215771be17afa2684117363
Черданцев Роман Андреевич <bandikost@gmail.com>
2026-04-16 14:59:35 +0300

    update

---
+++ .history/lib/mail_20260416144927.ts
@@ -0,0 +1,24 @@
+import * as nodemailer from "nodemailer"
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.yandex.ru",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, 
+  },
+})
+export async function sendWelcomeEmail(email: string, name: string) {
+  await transporter.sendMail({
+    from: `"Academy" <${process.env.EMAIL_USER}>`,
+    to: email,
+    subject: "Вы успешно прошли регистрацию",
+    html: `
+      <div>
+        <h2>Привет, ${name}</h2>
+        <p>Вы успешно зарегистрировались в системе.</p>
+      </div>
+    `,
+  })
+}
