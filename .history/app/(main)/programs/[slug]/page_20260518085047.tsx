7f85978874e3f3e0af3b3416d9d28f7feab6330f
Черданцев Роман Андреевич <bandikost@gmail.com>
2026-05-18 09:23:55 +0300

    update

--- .history/app/(main)/programs/[slug]/page_20260518085047.tsx
+++ .history/app/(main)/programs/[slug]/page_20260518085047.tsx
@@ -70,12 +70,12 @@ export default async function Page({ params }: ProgramsPageProps) {
 
   blocks.push({
     title: currentTitle?.blocks[2] || "",
-    component:  <Second />,
+    component:  <Second program={program} />,
   })
 
   blocks.push({
     title: currentTitle?.blocks[3] || "",
-    component: <Three />,
+    component: <Three program={program}/>,
   })
 
   blocks.push({
--- .history/app/api/activity-users/route_20260422130641.ts
+++ .history/app/api/activity-users/route_20260422130641.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
-import { ResultSetHeader } from "mysql2";
+import { ResultSetHeader } from "mysql2/promise";
 
 export async function POST(req: NextRequest) {
   try {
