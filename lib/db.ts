d750ea136a4bcfac120b315565f3b19d55dbea30
unknown <bandikost@gmail.com>
2026-06-03 10:08:38 +0300

    update admin dashboard

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,10 @@
 import mysql from "mysql2/promise";
 
-export const db = mysql.createPool({
+declare global {
+  var _mysqlPool: mysql.Pool | undefined;
+}
+
+export const db = mysql.createPool ({
   host: process.env.DB_HOST!,
   user: process.env.DB_USER!,
   password: process.env.DB_PASSWORD!,
@@ -11,5 +15,4 @@ export const db = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10, 
   queueLimit: 0
-});
-
+})
