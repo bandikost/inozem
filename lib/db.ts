1ec27d0710cfd32f1bfe9b5b8fd194af0e3add5e
unknown <bandikost@gmail.com>
2026-06-03 11:00:19 +0300

    update admin dashboard

--- lib/db.ts
+++ lib/db.ts
@@ -1,24 +1,27 @@
 import mysql from "mysql2/promise";
 
-declare global {
-  var _mysqlPool: mysql.Pool | undefined;
-}
-
-const db = mysql.createPool ({
+export const db = mysql.createPool({
   host: process.env.DB_HOST!,
   user: process.env.DB_USER!,
   password: process.env.DB_PASSWORD!,
   database: process.env.DB_NAME!,
+
   charset: "utf8mb4",
   decimalNumbers: true,
   timezone: "+00:00",
+
   waitForConnections: true,
-  connectionLimit: 10, 
-  queueLimit: 0
-})
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-const conn = await db.getConnection();
-await conn.query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
-conn.release();
+export async function query(sql: string, params?: any[]) {
+  const conn = await db.getConnection();
 
-export { db };
+  try {
+    await conn.query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
+    return await conn.query(sql, params);
+  } finally {
+    conn.release();
+  }
+}
