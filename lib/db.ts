54e9c74169be8ad95aa4a9ebd79cba5d52c049d6
unknown <bandikost@gmail.com>
2026-06-03 09:52:26 +0300

    update admin dashboard

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,6 @@
 import mysql from "mysql2/promise";
 
-declare global {
-  var _mysqlPool: mysql.Pool | undefined;
-}
-
-const pool: mysql.Pool = global._mysqlPool ?? mysql.createPool({
+export const pool = mysql.createPool({
   host: process.env.DB_HOST!,
   user: process.env.DB_USER!,
   password: process.env.DB_PASSWORD!,
@@ -17,12 +13,3 @@ const pool: mysql.Pool = global._mysqlPool ?? mysql.createPool({
   queueLimit: 0
 });
 
-if (!global._mysqlPool) {
-  global._mysqlPool = pool;
-
-  pool.on("connection", async (conn) => {
-    await conn.query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
-  });
-}
-
-export const db = pool;
