194453a0af772edcd68e01306fd8acdaaab13923
bandikost <bandikost@gmail.com>
2026-06-05 00:26:18 +0300

    1

--- lib/db.ts
+++ lib/db.ts
@@ -1,20 +1,19 @@
 import mysql from "mysql2/promise";
 
-const globalForMysql = globalThis as {
-  pool?: mysql.Pool;
-};
-
-export const db =
-  globalForMysql.pool ??
-  mysql.createPool({
-    host: process.env.DB_HOST!,
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_NAME!,
-    connectionLimit: 30,
-    waitForConnections: true,
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForMysql.pool = db;
+declare global {
+  var _mysqlPool: mysql.Pool | undefined;
 }
+
+export const db = mysql.createPool ({
+  host: process.env.DB_HOST!,
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASSWORD!,
+  database: process.env.DB_NAME!,
+  charset: "utf8mb4",
+  decimalNumbers: true,
+  timezone: "+00:00",
+  waitForConnections: true,
+  connectionLimit: 30, 
+  queueLimit: 0
+})
+
