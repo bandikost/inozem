2606b1f62990f694b6ceae02d50826cb827d31c8
bandikost <bandikost@gmail.com>
2026-06-05 11:39:10 +0300

    1

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,12 @@
+
+
 import mysql from "mysql2/promise";
 
 declare global {
   var _mysqlPool: mysql.Pool | undefined;
 }
 
-export const db = mysql.createPool ({
+const db = mysql.createPool ({
   host: process.env.DB_HOST!,
   user: process.env.DB_USER!,
   password: process.env.DB_PASSWORD!,
@@ -17,3 +19,8 @@ export const db = mysql.createPool ({
   queueLimit: 0
 })
 
+const conn = await db.getConnection();
+await conn.query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
+conn.release();
+
+export { db };
