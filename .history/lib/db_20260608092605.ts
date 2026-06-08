7e750340e54c0a4547f613b2d6a32ece90857a17
Черданцев Роман Андреевич <bandikost@gmail.com>
2026-06-08 09:29:18 +0300

    update

--- .history/lib/db_20260608092605.ts
+++ .history/lib/db_20260608092605.ts
@@ -20,9 +20,9 @@ const pool: mysql.Pool = global._mysqlPool ?? mysql.createPool({
 if (!global._mysqlPool) {
   global._mysqlPool = pool;
 
-  pool.on("connection", (conn) => {
-  conn.query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
-})
+  pool.on("connection", async (conn) => {
+    await conn.query("SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci");
+  });
 }
 
 export const db = pool;
