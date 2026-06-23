a7585a5445a37f2babf2d53da772549df3301010
Черданцев Роман Андреевич <bandikost@gmail.com>
2026-06-23 09:49:38 +0300

    update

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic"
+export const revalidate = 3600
 
 import FirstBlock from "@/components/MainPage/FirstBlock";
 import Programms from "@/components/MainPage/Programms";
