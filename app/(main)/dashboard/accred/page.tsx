a107a79176b46e015afc9269da00ecf2d5ad0de9
bandikost <bandikost@gmail.com>
2026-08-27 18:32:46 +0300

    update august

--- app/(main)/dashboard/accred/page.tsx
+++ app/(main)/dashboard/accred/page.tsx
@@ -1,16 +1,11 @@
 import LoadingLink from "@/components/Load/LoadingLink";
 import { ChevronRight } from "lucide-react";
-import { cookies } from "next/headers";
 import Link from "next/link";
-import { redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
 
 export default async function Page() {
-    const cookieStore = await cookies()
-    const manager = cookieStore.get("manager")  
-    if (!manager) redirect("/dashboard")
 
     return (
         <section className="max-w-6xl mx-auto px-6 mt-34">
--- app/(main)/dashboard/admin/programs/[slug]/page.tsx
+++ app/(main)/dashboard/admin/programs/[slug]/page.tsx
@@ -3,8 +3,6 @@
 import { getProgramBlocks, getProgramBySlug } from "@/lib/programm";
 import ProgramEditor from "../../components/ProgramEditor";
 import { ChevronRight } from "lucide-react";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import LoadingLink from "@/components/Load/LoadingLink";
 
 
@@ -17,11 +15,6 @@ export const metadata = {
 }
 
 export default async function AdminProgramsPage({params} : ProgramsPageProps) {
-    const cookieStore = await cookies()
-    const manager = cookieStore.get("manager")
-      
-    if (!manager) redirect("/dashboard")
-
     const {slug} = await params
     const program = await getProgramBySlug(slug)
     if (!program) return null
